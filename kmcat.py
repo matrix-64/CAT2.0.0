9d2e1873828cb174ef810ec13adb352adca8b4b7
김주완 <105963883+matrix-64@users.noreply.github.com>
2023-06-18 23:23:13 +0900

    Update kmcat.py

--- kmcat.py
+++ kmcat.py
@@ -1,11 +1,5 @@
-import pyautogui as pg
+import pynput
 
 class keyboardModeCAT:
-    Keys = {
-    
     def __init__(self,i_shape):
-        self.typing = False
-
-    def action(self,sh):
-        inp = sh[0]*32 + sh[1]
-        
+        pass
